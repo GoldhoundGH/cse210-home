2494634cb57d7d4bdc0cdb8dd865616660a09af3
GoldhoundGH <mb@micahbradford.com>
2026-09-24 10:25:04 -0600

    Fulfilled the W01 Prove Assignment on Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the student's grade? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter;
+        if (grade < 60)
+        {
+            letter = "F";
+        }
+        else if (grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade < 90)
+        {
+            letter = "B";
+        }
+        else
+        {
+            letter = "A";
+        }
+
+        string message;
+        if (grade >= 70)
+        {
+            message = "You passed the class. Good job.";
+        }
+        else
+        {
+            message = "You did not pass the class. Better luck next time.";
+        }
+
+        Console.WriteLine($"\nYour letter grade is '{letter}'. {message}");
     }
 }
